41e3274565b8c190e8902f1cbbd366eb1bd16ddd
CRImier <crimier@yandex.ru>
2018-02-09 05:01:01 +0200

    Experimenting continues (this does not actually work, and is not faster than original implementation either)

--- output/drivers/sh1106.py
+++ output/drivers/sh1106.py
@@ -6,39 +6,49 @@ from luma.oled.device import sh1106
 
 from output.output import OutputDevice
 
-#Monkey-patching SH1106 display function to allow for quasi-"diff-to-previous" redraws
-def partial_display(self, image):
+from bitarray import bitarray
+from array import array
+
+#Monkey-patching SH1106 display function to allow for faster buffer conversions
+def fast_display(self, image):
     assert(image.mode == self.mode)
     assert(image.size == self.size)
 
-    #image = self.preprocess(image)
+    image = self.preprocess(image)
 
     set_page_address = 0xB0
     image_data = image.getdata()
     pixels_per_page = self.width * 8
-    buf = bytearray(self.width)
+    buf = bitarray(pixels_per_page)
+    buf.setall(0)
 
-    for y in range(0, int(self._pages * pixels_per_page), pixels_per_page):
-        previous_buf = self.previous_buffers[y/pixels_per_page]
+    image_bitarray = bitarray(image_data)
+    #imgdata = (chr(i) for i in image_data)
+    #image_bitarray.extend(imgdata)
+
+    #for y in range(0, int(self._pages * pixels_per_page), pixels_per_page):
+    for y in range(8):
         self.command(set_page_address, 0x02, 0x10)
         set_page_address += 1
-        offsets = [y + self.width * i for i in range(8)]
+        start = y*pixels_per_page
+        for i in range(pixels_per_page):
+            r = i >> 3
+            c = i - (r << 3)
+            buf[c*8 + r] = image_bitarray[start+i]
+        #offsets = [y + self.width * i for i in range(8)]
 
-        for x in range(self.width):
-            buf[x] = \
-                (image_data[x + offsets[0]] and 0x01) | \
-                (image_data[x + offsets[1]] and 0x02) | \
-                (image_data[x + offsets[2]] and 0x04) | \
-                (image_data[x + offsets[3]] and 0x08) | \
-                (image_data[x + offsets[4]] and 0x10) | \
-                (image_data[x + offsets[5]] and 0x20) | \
-                (image_data[x + offsets[6]] and 0x40) | \
-                (image_data[x + offsets[7]] and 0x80)
-        if buf != previous_buf:
-             self.data(list(buf))
-        self.previous_buffers[y/pixels_per_page] = buf
-
-
+        #for x in range(self.width):
+        #    buf[x] = \
+        #        (image_data[x + offsets[0]] and 0x01) | \
+        #        (image_data[x + offsets[1]] and 0x02) | \
+        #        (image_data[x + offsets[2]] and 0x04) | \
+        #        (image_data[x + offsets[3]] and 0x08) | \
+        #        (image_data[x + offsets[4]] and 0x10) | \
+        #        (image_data[x + offsets[5]] and 0x20) | \
+        #        (image_data[x + offsets[6]] and 0x40) | \
+        #        (image_data[x + offsets[7]] and 0x80)
+        intarray = array('b', buf.tobytes())
+        self.data(list(intarray))
 
 
 class Screen(LumaScreen, OutputDevice):
@@ -51,5 +61,4 @@ class Screen(LumaScreen, OutputDevice):
     def init_display(self, autoscroll=False, **kwargs):
         """Initializes SH1106 controller. """
         self.device = sh1106(self.serial, width=128, height=64)
-        self.device.previous_buffers = [bytearray(self.device.width) for i in range(self.device._pages)]
-        self.device.display = MethodType(partial_display, self.device)
+        #self.device.display = MethodType(fast_display, self.device)
