7844b3069d10bf3402160ea51a7302c0541bdc9e
Timothée Floure <timothee.floure@fnux.ch>
2019-01-03 20:17:21 +0100

    Contacts App: add vdirsyncer CardDAV remote setup wizard

--- apps/personal/contacts/main.py
+++ apps/personal/contacts/main.py
@@ -8,19 +8,20 @@ from address_book import AddressBook, ZPUI_HOME
 from contact import Contact
 from apps import ZeroApp
 from helpers import setup_logger
-from ui import NumberedMenu, Listbox, Menu, LoadingIndicator, Printer, DialogBox
+from ui import (NumberedMenu, Listbox, Menu, LoadingIndicator, Printer,
+                DialogBox, UniversalInput)
 from distutils.spawn import find_executable
+from helpers.vdirsyncer import (vdirsyncer_sync, vdirsyncer_discover,
+                                vdirsyncer_set_carddav_remote,
+                                vdirsyncer_get_storage_directory_for,
+                                vdirsyncer_generate_config)
 
-logger = setup_logger(__name__, "info")
-
-# FIXME: load from config module?
-VDIRSYNCER_CONFIG = '~/.config/vdirsyncer/config'
-VDIRSYNCER_VCF_DIRECOTRY = '~/.local/share/vdirsyncer/contacts/contacts'
+logger = setup_logger(__name__, 'info')
 
 class ContactApp(ZeroApp):
     def __init__(self, i, o):
         super(ContactApp, self).__init__(i, o)
-        self.menu_name = "Contacts"
+        self.menu_name = 'Contacts'
         self.address_book = AddressBook()
         self.menu = None
         self.vdirsyncer_executable = find_executable('vdirsyncer')
@@ -37,7 +38,7 @@ class ContactApp(ZeroApp):
     def build_main_menu_content(self):
         all_contacts = self.address_book.contacts
         menu_entries = []
-        menu_entries.append(["|| Actions", lambda: self.open_actions_menu()])
+        menu_entries.append(['|| Actions', lambda: self.open_actions_menu()])
         for c in all_contacts:
             menu_entries.append([c.short_name(), lambda x=c:
                                  self.open_contact_details_page(x)])
@@ -52,33 +53,20 @@ class ContactApp(ZeroApp):
 
     def open_actions_menu(self):
         menu_contents = [
-            ["Settings", lambda: self.open_settings_page()],
-            ["CardDAV Import", lambda: self.synchronize_carddav()],
-            ["Reset address book", lambda: self.reset_addressbook()]
+            ['CardDAV Setup Wizard', lambda: self.open_remote_setup_wizard()],
+            ['CardDAV Sync', lambda: self.synchronize_carddav()],
+            ['Reset address book', lambda: self.reset_addressbook()]
         ]
-        Menu(menu_contents, i=self.i, o=self.o, name="My menu").activate()
-
-    def open_settings_page(self):
-        if self.vdirsyncer_executable:
-            vdirsyncer_executable = self.vdirsyncer_executable
-        else:
-            vdirsyncer_executable = 'Not found'
-
-        attrs = [
-            ["-- VdirSyncer"],
-            ["Executable: {}".format(vdirsyncer_executable) ],
-            ["Config: {}".format(VDIRSYNCER_CONFIG)]
-        ]
-        Listbox(i=self.i, o=self.o, contents=attrs).activate()
+        Menu(menu_contents, i=self.i, o=self.o, name='My menu').activate()
 
     def reset_addressbook(self):
-        alert = "This action will delete all of your contacts. Are you sure?"
+        alert = 'This action will delete all of your contacts. Are you sure?'
         do_reset = DialogBox('yc', i=self.i, o=self.o, message=alert,
-                             name="Address book reset").activate()
+                             name='Address book reset').activate()
 
         if do_reset:
             self.address_book.reset()
-            announce = "All of your contacts were deleted."
+            announce = 'All of your contacts were deleted.'
             Printer(announce, i=self.i, o=self.o, sleep_time=2, skippable=True)
             # Reload the now empty address book
             self.reload()
@@ -91,36 +79,73 @@ class ContactApp(ZeroApp):
             callback()
             return;
 
-        vdirsyncer_command = "{} -c {} sync contacts".format(
-            self.vdirsyncer_executable, VDIRSYNCER_CONFIG
-        )
-        logger.info("Calling vdirsyncer to synchronize contacts")
-        with LoadingIndicator(self.i, self.o, message="Syncing contacts"):
-            exit_status = os.system(vdirsyncer_command)
+        with LoadingIndicator(self.i, self.o, message='Syncing contacts'):
+            exit_status = vdirsyncer_sync('contacts')
 
         if (exit_status != 0):
-            error_msg = "Error in contact synchronization. Did you configure \
-            vdirsyncer?"
+            error_msg = "Error in contact synchronization. See ZPUI logs for \
+            details."
             Printer(error_msg, i=self.i, o=self.o, sleep_time=3,
                     skippable=True)
             self.open_actions_menu()
 
-        with LoadingIndicator(self.i, self.o, message="Importing contacts"):
-            self.address_book.import_vcards_from_directory(VDIRSYNCER_VCF_DIRECOTRY)
+        with LoadingIndicator(self.i, self.o, message='Importing contacts'):
+            self.address_book.import_vcards_from_directory(
+                vdirsyncer_get_storage_directory_for('contacts')
+            )
             self.address_book.save_to_file()
 
         # Reload the synced address book
         self.reload()
 
+    def open_remote_setup_wizard(self):
+        # Define wizard fields
+        url_field = UniversalInput(self.i, self.o,
+                                   message='CardDAV URL:',
+                                   name='CardDAV URL field')
+        username_field = UniversalInput(self.i, self.o,
+                                        message='CardDAV Username:',
+                                        name='CardDAV username field')
+        password_field = UniversalInput(self.i, self.o,
+                                        message='CardDAV Password:',
+                                        name='CardDAV password field')
+
+        # Run wizard
+        url = url_field.activate()
+        username = username_field.activate()
+        password = password_field.activate()
+
+        # Update ZPUI vdirsyncer config, generate vdirsyncer config file
+        vdirsyncer_set_carddav_remote(url, username, password)
+        vdirsyncer_generate_config()
+
+        # Initialize vdirsyncer remote
+        with LoadingIndicator(self.i, self.o, message='Initializing remote'):
+            exit_status = vdirsyncer_discover('contacts')
+
+        if (exit_status != 0):
+            error_msg = "Error in remote initialization. Check vdirsyncer \
+            configuration"
+            Printer(error_msg, i=self.i, o=self.o, sleep_time=3,
+                    skippable=True)
+            return
+
+        # Synchronize contacts if the user request it
+        sync_now = DialogBox('yn', self.i, self.o,
+                             message='Remote saved. Sync now?',
+                             name='Sync synced contacts').activate()
+        if sync_now: self.synchronize_carddav()
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--src-folder', dest='src_folder', action='store', metavar="DIR",
+    logger.info('Generating vdirsyncer configuration')
+    parser.add_argument('-i', '--src-folder', dest='src_folder', action='store', metavar='DIR',
                         help='Folder to read vcard from', default=ZPUI_HOME)
     parser.add_argument('-t', '--run-tests', dest='test', action='store_true', default=False)
     arguments = parser.parse_args()
 
     if arguments.test:
-        logger.info("Running tests...")
+        logger.info('Running tests...')
         doctest.testmod()
 
     address_book = AddressBook()
---
+++ helpers/vdirsyncer.py
@@ -0,0 +1,109 @@
+from jinja2 import Template, Environment, FileSystemLoader
+from helpers.config_parse import write_config
+from helpers.xdg import XDG_CONFIG_HOME, XDG_DATA_HOME
+from main import load_config
+import os
+
+from helpers.logger import setup_logger
+logger = setup_logger(__name__, "info")
+
+DEFAULT_VDIRSYNCER_BINARY = '/usr/bin/vdirsyncer'
+DEFAULT_VDIRSYNCER_CONFIG_FILE = os.path.join(XDG_CONFIG_HOME,
+                                              'vdirsyncer',
+                                              'zp_config')
+#DEFAULT_VDIRSYNCER_STORAGE_DIRECTORY = os.path.join(XDG_DATA_HOME, 'vdirsyncer')
+DEFAULT_VDIRSYNCER_STORAGE_DIRECTORY = os.path.join('/tmp', 'vdirsyncer')
+
+def vdirsyncer_sync(vdirsyncer_pair):
+    return vdirsyncer_execute(['sync', vdirsyncer_pair])
+
+def vdirsyncer_discover(vdirsyncer_pair):
+    return vdirsyncer_execute(['discover', vdirsyncer_pair])
+
+def vdirsyncer_execute(vdirsyncer_args):
+    zp_config = load_config()[0]
+    zp_vdirsyncer_config = zp_config.get('vdirsyncer', {})
+    vdirsyncer_binary = zp_vdirsyncer_config.get(
+        'binary', DEFAULT_VDIRSYNCER_BINARY
+    )
+    vdirsyncer_config_file = zp_vdirsyncer_config.get(
+        'config_file', DEFAULT_VDIRSYNCER_CONFIG_FILE
+    )
+
+    vdirsyncer_command = "{} -c {}".format(vdirsyncer_binary,
+                                           vdirsyncer_config_file)
+    vdirsyncer_command += ' ' + ' '.join(vdirsyncer_args)
+
+    logger.info("External binary call: {}".format(vdirsyncer_command))
+    return os.system(vdirsyncer_command)
+
+# TODO: add support for multiple remotes
+def vdirsyncer_set_carddav_remote(url, username, password):
+    zp_config, zp_config_path = load_config()
+    zp_vdirsyncer_config = zp_config.get('vdirsyncer', {})
+    contacts_storage_directory = vdirsyncer_get_storage_directory_for('contacts')
+
+    zp_vdirsyncer_config['contacts'] = {
+        'pair': {
+            'a': 'contacts_local',
+            'b': 'contacts_remote',
+            'collections': 'null'
+        },
+        'local': {
+            'type': 'filesystem',
+            'path': contacts_storage_directory, 
+            'fileext': '.vcf'
+        },
+        'remote': {
+            'type': 'carddav',
+            'url': url,
+            'username': username,
+            'password': password
+        }
+    }
+
+    zp_config['vdirsyncer'] = zp_vdirsyncer_config
+    return write_config(zp_config, zp_config_path)
+
+def vdirsyncer_get_storage_directory_for(pair):
+    zp_config = load_config()[0]
+    zp_vdirsyncer_config = zp_config.get('vdirsyncer', {})
+    vdirsyncer_storage_directory = zp_vdirsyncer_config.get(
+        'storage_directory', DEFAULT_VDIRSYNCER_STORAGE_DIRECTORY
+    )
+
+    directory = os.path.join(vdirsyncer_storage_directory, pair)
+
+    # Create the directory ourself so that vdirsyncer does not ask stdin!
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    return directory
+
+# TODO: similar to vdirsyncer_set_carddav_remote
+def vdirsyncer_set_calddav_remote():
+    return
+
+def vdirsyncer_generate_config():
+    zp_config = load_config()[0]
+    zp_vdirsyncer_config = zp_config.get('vdirsyncer', {})
+    status_directory = vdirsyncer_get_storage_directory_for('_vdirsyncer_status')
+
+    logger.info('Generating vdirsyncer configuration')
+
+    jinja2_env = Environment(loader=FileSystemLoader('helpers/vdirsyncer'),
+                             trim_blocks=True)
+    template = jinja2_env.get_template('config.j2')
+    rendered_vdirsyncer_config = template.render(
+        contacts=zp_vdirsyncer_config['contacts'],
+        status_directory=status_directory
+    )
+
+    vdirsyncer_config_file = zp_vdirsyncer_config.get(
+        'config_file', DEFAULT_VDIRSYNCER_CONFIG_FILE
+    )
+
+    logger.info('Writing vdirsyncer configuration')
+    with open(vdirsyncer_config_file, 'w') as fh:
+        fh.write(rendered_vdirsyncer_config)
+        fh.close
