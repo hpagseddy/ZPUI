ed9c69072a782d6ecf48e50b7c23acdb188b1c13
CRImier <crimier@yandex.ru>
2019-06-03 08:56:53 +0300

    Zone implementation + example app done (for main screen)

---
+++ apps/example_apps/zone_test/main.py
@@ -0,0 +1,107 @@
+from datetime import datetime
+from time import sleep
+from copy import copy
+
+from ui.status import Zone, ZoneSpacer as ZS, VerticalZoneSpacer as VZS, ZoneManager
+from ui import MockOutput, Canvas
+
+from helpers import setup_logger
+
+logger = setup_logger(__name__, "info")
+
+icon_canvas = Canvas(MockOutput(22, 8))
+hh_mm_canvas = Canvas(MockOutput(80, 30))
+ss_canvas = Canvas(MockOutput(20, 10))
+button_canvas = Canvas(MockOutput(40, 8))
+
+i = None
+o = None
+
+counter = 0
+
+def is_charging():
+    return counter % 2
+
+def get_battery():
+    global counter
+    counter += 1
+    return counter
+
+def draw_battery(value):
+    icon_canvas.clear()
+    icon_canvas.rectangle((2, 0, 20, 7))
+    icon_canvas.rectangle((6, 0, 20, 7), fill="white")
+    icon_canvas.rectangle((0, 1, 2, 6), fill="white")
+    if is_charging():
+        icon_canvas.text("Ch", (7, -1), fill="black")
+    return copy(icon_canvas.get_image())
+
+def get_gsm():
+    return counter
+
+def draw_gsm(value):
+    icon_canvas.clear()
+    icon_canvas.line((3, 1, 3, 6))
+    icon_canvas.line((1, 1, 5, 1))
+    icon_canvas.point(((1, 2), (5, 2)))
+    offset = 7
+    for x in reversed(range(6)):
+        icon_canvas.line((x*2+offset, 6-x, x*2+offset, 6))
+    return copy(icon_canvas.get_image())
+
+def get_hh_mm():
+    return datetime.now().strftime("%H:%M")
+
+def get_ss():
+    return datetime.now().strftime("%S")
+
+def draw_hh_mm(value):
+    hh_mm_canvas.clear()
+    hh_mm_canvas.text(value, (0, 0), font=("Fixedsys62.ttf", 32))
+    return hh_mm_canvas.get_image()
+
+def draw_ss(value):
+    ss_canvas.clear()
+    ss_canvas.text(value, (0, 0))
+    return ss_canvas.get_image()
+
+def draw_button(text):
+    button_canvas.clear()
+    button_canvas.centered_text(text)
+    return copy(button_canvas.get_image())
+
+def get_button_l():
+    return "Hello"
+
+def get_button_r():
+    return "World"
+
+hh_mm_z = Zone(get_hh_mm, draw_hh_mm, name="HH_MM")
+ss_z = Zone(get_ss, draw_ss, name="SS")
+button_l_z = Zone(get_button_l, draw_button, name="lbutton")
+button_r_z = Zone(get_button_r, draw_button, name="rbutton")
+battery_z = Zone(get_battery, draw_battery, name="battery")
+gsm_z = Zone(get_gsm, draw_gsm, name="gsm")
+
+zones = {"hh_mm":hh_mm_z,
+         "ss":ss_z,
+         "gsm":gsm_z,
+         "battery":battery_z,
+         "button_l":button_l_z,
+         "button_r":button_r_z}
+
+markup = [
+  [VZS(1)],
+  [ZS(1), "gsm", "...", "battery"],
+  [VZS(2)],
+  ["hh_mm", ZS(3), "ss"],
+  ["..."],
+  ["...", "button_l", "...", "button_r", "..."],
+  [VZS(1)]
+]
+
+def callback():
+    zm = ZoneManager(i, o, markup, zones)
+    for x in range(5):
+        zm.show()
+        sleep(1)
--- ui/status.py
+++ ui/status.py
@@ -1,5 +1,9 @@
 from canvas import Canvas
+from base_ui import BaseUIElement
 
+from helpers import setup_logger
+
+logger = setup_logger(__name__, "info")
 """
 Zone requirements:
 - get values from some kind of source
@@ -20,14 +24,14 @@ class Zone(object):
     value = None
     image = None
     prev_value = None
-    cache = {}
 
-    def __init__(self, value_cb, image_cb, caching = True, i_pass_self = False, v_pass_self = False):
+    def __init__(self, value_cb, image_cb, caching = True, i_pass_self = False, v_pass_self = False, name=None):
         self.value_cb = value_cb
         self.image_cb = image_cb
         self.caching = caching
         self.v_pass_self = v_pass_self
         self.i_pass_self = i_pass_self
+        self.name = name
         if self.caching:
             self.cache = {}
 
@@ -38,6 +42,7 @@ class Zone(object):
         else:
             new_value = self.value_cb()
         if new_value != self.value:
+            logger.debug("Zone {}: new value {}".format(self.name, new_value))
             # New value!
             self.prev_value = self.value
             self.value = new_value
@@ -65,23 +70,109 @@ class Zone(object):
     def refresh(self):
         if self.needs_refresh():
             self.image = self.update_image()
+            return True
+        return False
 
+class ZoneSpacer(object):
+    def __init__(self, value):
+        self.value = value
 
-class ZoneManager(object):
+class VerticalZoneSpacer(ZoneSpacer):
+    pass
 
-    def __init__(self, zones):
+class ZoneManager(BaseUIElement):
+
+    def __init__(self, i, o, markup, zones, name="ZoneManager", **kwargs):
         self.zones = zones
+        self.markup = markup
+        self.zones_that_need_refresh = {}
+        self.refresh_on_start()
+        self.name = name
+        self.c = Canvas(o)
+        self.o = o
+        self.i = i
+        #BaseUIElement.__init__(self, i, o, name, **kwargs)
 
-    def refresh(self):
-        for zone in self.zones.values():
+    def refresh_on_start(self):
+        for name, zone in self.zones.items():
             zone.refresh()
+            self.zones_that_need_refresh[name] = True
 
+    def get_element_height(self, element):
+        if element in self.zones:
+            return self.zones[element].get_image().height
+        elif isinstance(element, VerticalZoneSpacer):
+            return element.value
 
+    def get_element_width(self, element):
+        if element in self.zones:
+            return self.zones[element].get_image().width
+        elif isinstance(element, ZoneSpacer) \
+         and not isinstance(element, VerticalZoneSpacer):
+            return element.value
 
-markup = [
-  ("gsm", "...", "battery"),
-  ("..."),
-  ("time_hm", "time_s"),
-  ("..."),
-  ("...", "b1", "...", "b2", "...")
-]
+    def show(self):
+        row_heights = []
+        for row in self.markup:
+            for item in row:
+                if item in self.zones:
+                    zone = self.zones[item]
+                    has_refreshed = zone.refresh()
+                    if has_refreshed and not self.zones_that_need_refresh[item]:
+                        self.zones_that_need_refresh[item] = True
+                else:
+                    if isinstance(item, basestring):
+                        if not all([char == "." for char in item]):
+                            logger.warning("Zone {} unknown!".format(item))
+                    elif isinstance(item, (ZoneSpacer, VerticalZoneSpacer)):
+                        pass
+            item_heights = [self.get_element_height(item) for item in row]
+            if filter(None, item_heights):
+                row_height = max(item_heights)
+            else:
+                row_height = None
+            row_heights.append(row_height)
+        # Calculating vertical spacing between rows
+        empty_row_amount = row_heights.count(None)
+        if empty_row_amount == 0:
+            pass # will just leave empty space at the bottom
+        else: # One or more empty rows
+            # Let's redistribute the empty space equally
+            empty_space = self.o.height-sum(filter(None, row_heights))
+            # Unless there's no space to redistribute, that is
+            if empty_space > 0:
+                for i, el in enumerate(row_heights):
+                    spacing = int(empty_space/empty_row_amount)
+                    if el is None:
+                        row_heights[i] = spacing
+        # Redrawing the elements (only those we need to redraw)
+        y = 0
+        for i, row in enumerate(self.markup):
+            row_height = row_heights[i]
+            x = 0
+            item_widths = [self.get_element_width(item) for item in row]
+            # Calculating horizontal spacing between items
+            empty_item_amount = item_widths.count(None)
+            if empty_item_amount == 0:
+                pass # will just leave empty space on the right
+            else: # One or more empty items
+                # Let's redistribute the empty space equally
+                empty_space = self.o.width-sum(filter(None, item_widths))
+                # Unless there's no space to redistribute, that is
+                if empty_space > 0:
+                    spacing = int(empty_space/empty_item_amount)
+                    for i, el in enumerate(item_widths):
+                        if el is None:
+                            item_widths[i] = spacing
+            for i, item in enumerate(row):
+                width = item_widths[i]
+                if item not in self.zones:
+                    x += width
+                    continue
+                image = self.zones[item].get_image()
+                if self.zones_that_need_refresh[item]:
+                    self.c.paste(image, (x, y))
+                    self.zones_that_need_refresh[item] = False
+                x += width
+            y += row_height
+        self.c.display()
